16579f75a72e89990fb2435db2ca3c39276d0814
manoreken <manoreken2@gmail.com>
2020-12-30 16:17:23 +0900

    PngToBmp full 16-bit precision conversion

--- PngToBmp/MLBmpWriter.cpp
+++ PngToBmp/MLBmpWriter.cpp
@@ -15,10 +15,11 @@ FloatToUint16(float v) {
 }
 
 /// <summary>
-/// 48bit BMPファイルを書き込む。
+/// 24bit BMP または 48bit BMPファイルを書き込む。
 /// </summary>
 /// <returns>0:成功。負の数:失敗。</returns>
-int MLBmpWrite(const char* filePath, MLImage& img) {
+int MLBmpWrite(const char* filePath, MLImage& img)
+{
     FILE* fp = nullptr;
     int ercd = fopen_s(&fp, filePath, "wb");
     if (ercd != 0 || fp == nullptr) {
@@ -26,39 +27,76 @@ int MLBmpWrite(const char* filePath, MLImage& img) {
         return E_FAIL;
     }
 
-    // 48bit BGR画像データ作成。
-    int imgBytes = img.width * img.height * 3 * 2; // 3==numCh, 2 == bytesPerChannel
-    uint16_t* imgTo = new uint16_t[imgBytes];
-    const half* imgFrom = (const half*)img.data;
-    for (int y = 0; y < img.height; ++y) {
-        for (int x = 0; x < img.width; ++x) {
-            int readP = (x + y * img.width) * 4;
-            int writeP = (x + (img.height - y - 1) * img.width) * 3;
+    int64_t imgBytes = 0;
+    uint8_t* imgTo = nullptr;
+    int biBitCount = 0;
+    if (img.bitFormat == MLImage::BFT_UInt8) {
+        // 24bit BGR画像データ作成。
+        biBitCount = 24;
+        imgBytes = (int64_t)img.width * img.height * 3 * sizeof(uint8_t); // 3==書き込みデータのnumCh
+        imgTo = new uint8_t[imgBytes];
+        for (int y = 0; y < img.height; ++y) {
+            for (int x = 0; x < img.width; ++x) {
+                // imgにはRGBAの4チャンネルデータが左から右、上から下の順に入っている。
+                // 24bit BMPはBGR、左から右、下から上の順に書き込む。
+                int readP = (x + y * img.width) * 4;
+                int writeP = (x + (img.height - y - 1) * img.width) * 3;
 
-            uint16_t r = FloatToUint16(imgFrom[readP + 0]);
-            uint16_t g = FloatToUint16(imgFrom[readP + 1]);
-            uint16_t b = FloatToUint16(imgFrom[readP + 2]);
+                uint8_t r = img.data[readP + 0];
+                uint8_t g = img.data[readP + 1];
+                uint8_t b = img.data[readP + 2];
 
-            imgTo[writeP + 0] = r;
-            imgTo[writeP + 1] = g;
-            imgTo[writeP + 2] = b;
+                imgTo[writeP + 0] = b;
+                imgTo[writeP + 1] = g;
+                imgTo[writeP + 2] = r;
+            }
         }
+
+    } else if (img.originalBitDepth == 16) {
+        // 48bit RGB画像データ作成。
+        biBitCount = 48;
+
+        const uint16_t* imgFrom = (const uint16_t*)img.data;
+
+        imgBytes = (int64_t)img.width * img.height * 3 * sizeof(uint16_t); // 3==書き込みデータのnumCh
+        imgTo = new uint8_t[imgBytes];
+        uint16_t* imgTo16 = (uint16_t*)imgTo;
+        for (int y = 0; y < img.height; ++y) {
+            for (int x = 0; x < img.width; ++x) {
+                // imgにはRGBAの4チャンネルデータが左から右、上から下の順に入っている。
+                // 48bit BMPはRGB、左から右、下から上の順に書き込むとirfanviewで正しく表示される。
+                int readP = (x + y * img.width) * 4;
+                int writeP = (x + (img.height - y - 1) * img.width) * 3;
+
+                uint16_t r = imgFrom[readP + 0];
+                uint16_t g = imgFrom[readP + 1];
+                uint16_t b = imgFrom[readP + 2];
+
+                imgTo16[writeP + 0] = r;
+                imgTo16[writeP + 1] = g;
+                imgTo16[writeP + 2] = b;
+            }
+        }
+    } else {
+        // 作ってない。
+        printf("Error: MLBmpWrite Unsupported bitformat %d\n", img.bitFormat);
+        return E_NOTIMPL;
     }
 
     BITMAPFILEHEADER bmpfh = {};
     BITMAPINFOHEADER bmpih = {};
 
     bmpfh.bfType = 0x4d42;
-    bmpfh.bfSize = sizeof bmpfh + sizeof bmpih + imgBytes;
+    bmpfh.bfSize = (DWORD)(sizeof bmpfh + sizeof bmpih + imgBytes);
     bmpfh.bfOffBits = sizeof bmpfh + sizeof bmpih;
 
     bmpih.biSize = sizeof bmpih;
     bmpih.biWidth = img.width;
     bmpih.biHeight = img.height;
     bmpih.biPlanes = 1;
-    bmpih.biBitCount = 48; // 48bit BMP
+    bmpih.biBitCount = biBitCount;
     bmpih.biCompression = BI_RGB;
-    bmpih.biSizeImage = imgBytes;
+    bmpih.biSizeImage = (DWORD)imgBytes;
     bmpih.biXPelsPerMeter = 2835;
     bmpih.biYPelsPerMeter = 2835;
     bmpih.biClrUsed = 0;
