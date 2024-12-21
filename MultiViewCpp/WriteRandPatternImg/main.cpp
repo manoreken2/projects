0ca51d048a62785fc9e371931abbdfe9b61b5cfb
a <a@a>
2024-12-21 10:01:46 +0900

    random pattern image png genneration

--- MultiViewCpp/WriteRandPatternImg/main.cpp
+++ MultiViewCpp/WriteRandPatternImg/main.cpp
@@ -5,53 +5,74 @@
 #include <opencv2/ccalib/randpattern.hpp>
 
 static void
-PrintUsage(wchar_t* progName)
+PrintUsage(char* progName)
 {
-    printf("Usage: %S width height pngFileName\n", progName);
+    printf("Usage: %s numImg width height pngFileNameWithoutExt\n", progName);
+    printf("    numImg      : total image count to write\n");
     printf("    width       : image width in pixel\n");
     printf("    height      : image height in pixel\n");
-    printf("    pngFileName : png file name to write.\n");
+    printf("    pngFileNameWithoutExt : png file name (without extension) to write.\n");
 }
 
 static bool
-Run(int width,
+Run(int n,
+    int width,
     int height,
-    const wchar_t* pngFileName)
+    const char* pngFileNameWithoutExt)
 {
-    cv::randpattern::RandomPatternGenerator::RandomPatternGenerator rpg(
+    cv::randpattern::RandomPatternGenerator rpg(
         width, height);
 
-    rpg.generatePattern();
-    auto img = rpg.getPattern();
+    char pngFileName[_MAX_PATH];
 
+    for (int i = 0; i < n; ++i) {
+        sprintf_s(pngFileName, "%s%04d.png", pngFileNameWithoutExt, i);
+
+        rpg.generatePattern();
+        auto img = rpg.getPattern();
+
+        bool bRv = cv::imwrite(pngFileName, img);
+
+        if (!bRv) {
+            printf("Error: imwrite failed %s\n", pngFileName);
+            return false;
+        } else {
+            printf("Wrote %s\n", pngFileName);
+        }
+    }
 
     return true;
 }
 
 int
-wmain(int argc, wchar_t *argv[])
+main(int argc, char *argv[])
 {
-    if (argc != 4) {
+    if (argc != 5) {
         PrintUsage(argv[0]);
         return 1;
     }
 
-    int width = _wtoi(argv[1]);
+    int n = atoi(argv[1]);
+    if (n <= 0) {
+        PrintUsage(argv[0]);
+        return 1;
+    }
+
+    int width = atoi(argv[2]);
     if (width <= 0) {
         PrintUsage(argv[0]);
         return 1;
     }
 
-    int height = _wtoi(argv[2]);
+    int height = atoi(argv[3]);
     if (height <= 0) {
         PrintUsage(argv[0]);
         return 1;
     }
 
-    const wchar_t* pngFileName = argv[3];
+    const char* pngFileName = argv[4];
 
-
-    bool bRv = Run(width, height, pngFileName);
+    bool bRv = Run(n, width, height, pngFileName);
 
     return bRv ? 0 : 1;
 }
